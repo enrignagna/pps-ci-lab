b3e5ef1c5d8fdd72b5c7ba8545f82eabb703ce1e
Enrico Gnagnarella <enrico.gnagnarella@studio.unibo.it>
2020-05-09 16:30:27 +0200

    Last commit

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,6 @@
 plugins {
     java
+    scala
     application
 }
 repositories {
@@ -7,7 +8,14 @@ repositories {
 }
 dependencies {
     implementation("commons-io:commons-io:+")
+    testImplementation("org.junit.jupiter:junit-jupiter-api:5.6.0")
+    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine")
 }
+
+tasks.test {
+    useJUnitPlatform()
+}
+
 application {
     mainClassName = "src/main/java/Hello.java"
 }
